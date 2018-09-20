bab469d36c680b6bd7461a0cfb6368c7fa38888b
Maciek <maciek.dec@gmail.com>
2018-09-20 22:42:07 +0200

    'aaa'

--- BookieTamer/urls.py
+++ BookieTamer/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('analysis.urls')),
+
+    # in order to go to home if blank address
+    path('', RedirectView.as_view(url='home')),
 ]
