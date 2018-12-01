94b0f8e38cc977b580dbb0987fe16ac96f044ffd
Maciek <maciek.dec@gmail.com>
2018-12-01 11:43:19 +0100

    games ordering by date, cooef

--- datafetch/views.py
+++ datafetch/views.py
@@ -8,7 +8,7 @@ from datafetch.utils import get_fortuna_games
 def games_to_bet_list(request):
 
     df = get_fortuna_games()
-    html_table = df.sort_values('Match Day').to_html(index=False)
+    html_table = df.sort_values(by=['Match Day', 'X coef']).to_html(index=False)
 
     return render_to_response('datafetch/games_to_bet.html', {'html_table': html_table})
 
