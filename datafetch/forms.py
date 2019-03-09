3a638448a28c63dd825c67f1898d1425dcb4036b
maciek <maciek.dec@gmail.com>
2019-03-09 21:10:01 +0100

    base form template kitmondo like

--- datafetch/forms.py
+++ datafetch/forms.py
@@ -6,18 +6,30 @@ from core.forms import BootstrapStylingBaseForm
 
 class DataFetchSettingsForm(BootstrapStylingBaseForm, forms.ModelForm):
     field_cols = {
-        'date_offset': 4,
-        'difference_x_range_min': 2,
-        'difference_x_range_max': 2,
-        'odds_1_min': 2,
-        'odds_1_max': 2,
-        'odds_1_min_second': 2,
-        'odds_1_max_second':2,
+        'date_offset': 2,
+        'difference_x_range_min': 6,
+        'difference_x_range_max': 6,
+        'odds_1_min': 6,
+        'odds_1_max': 6,
+        'odds_1_min_second': 6,
+        'odds_1_max_second': 6,
     }
 
 
     fieldsets = (
-
+        {
+            'title': 'Time frame',
+            'icon': '',
+            'icon_color': '',
+            'fields': ('date_offset')
+        },
+        {
+            'title': 'Odds settings',
+            'icon': '',
+            'icon_color': '',
+            'fields': ('difference_x_range_min', 'difference_x_range_max', 'odds_1_min', 'odds_1_max',
+                       'odds_1_min_second', 'odds_1_max_second')
+         }
     )
 
     class Meta:
